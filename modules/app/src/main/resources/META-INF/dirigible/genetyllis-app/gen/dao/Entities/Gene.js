ca0cc5c337d708ced461541b2fa7cd0067daa354
Nedelcho Delchev <delchev@Nedelchos-MacBook-Pro.local>
2022-05-30 18:53:38 +0300

    app model initial

---
+++ modules/app/src/main/resources/META-INF/dirigible/genetyllis-app/gen/dao/Entities/Gene.js
@@ -0,0 +1,100 @@
+var query = require("db/v4/query");
+var producer = require("messaging/v4/producer");
+var daoApi = require("db/v4/dao");
+var EntityUtils = require("genetyllis-app/gen/dao/utils/EntityUtils");
+
+var dao = daoApi.create({
+	table: "GENE",
+	properties: [
+		{
+			name: "Id",
+			column: "GENE_ID",
+			type: "INTEGER",
+			id: true,
+			autoIncrement: true,
+		}, {
+			name: "Name",
+			column: "GENE_NAME",
+			type: "VARCHAR",
+		}, {
+			name: "Pseudo",
+			column: "GENE_PSEUDO",
+			type: "BOOLEAN",
+		}, {
+			name: "Region",
+			column: "GENE_REGION",
+			type: "VARCHAR",
+		}]
+});
+
+exports.list = function(settings) {
+	return dao.list(settings).map(function(e) {
+		EntityUtils.setBoolean(e, "Pseudo");
+		return e;
+	});
+};
+
+exports.get = function(id) {
+	var entity = dao.find(id);
+	EntityUtils.setBoolean(entity, "Pseudo");
+	return entity;
+};
+
+exports.create = function(entity) {
+	EntityUtils.setBoolean(entity, "Pseudo");
+	var id = dao.insert(entity);
+	triggerEvent("Create", {
+		table: "GENE",
+		key: {
+			name: "Id",
+			column: "GENE_ID",
+			value: id
+		}
+	});
+	return id;
+};
+
+exports.update = function(entity) {
+	EntityUtils.setBoolean(entity, "Pseudo");
+	dao.update(entity);
+	triggerEvent("Update", {
+		table: "GENE",
+		key: {
+			name: "Id",
+			column: "GENE_ID",
+			value: entity.Id
+		}
+	});
+};
+
+exports.delete = function(id) {
+	dao.remove(id);
+	triggerEvent("Delete", {
+		table: "GENE",
+		key: {
+			name: "Id",
+			column: "GENE_ID",
+			value: id
+		}
+	});
+};
+
+exports.count = function() {
+	return dao.count();
+};
+
+exports.customDataCount = function() {
+	var resultSet = query.execute("SELECT COUNT(*) AS COUNT FROM GENE");
+	if (resultSet !== null && resultSet[0] !== null) {
+		if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
+			return resultSet[0].COUNT;
+		} else if (resultSet[0].count !== undefined && resultSet[0].count !== null) {
+			return resultSet[0].count;
+		}
+	}
+	return 0;
+};
+
+function triggerEvent(operation, data) {
+	producer.queue("genetyllis-app/Entities/Gene/" + operation).send(JSON.stringify(data));
+}
---
+++ modules/app/src/main/resources/META-INF/dirigible/genetyllis-app/gen/dao/Entities/Population.js
@@ -0,0 +1,84 @@
+var query = require("db/v4/query");
+var producer = require("messaging/v4/producer");
+var daoApi = require("db/v4/dao");
+
+var dao = daoApi.create({
+	table: "POPULATION",
+	properties: [
+		{
+			name: "Id",
+			column: "POPULATION_ID",
+			type: "INTEGER",
+			id: true,
+			autoIncrement: true,
+		}, {
+			name: "Name",
+			column: "POPULATION_NAME",
+			type: "VARCHAR",
+		}]
+});
+
+exports.list = function(settings) {
+	return dao.list(settings);
+};
+
+exports.get = function(id) {
+	return dao.find(id);
+};
+
+exports.create = function(entity) {
+	var id = dao.insert(entity);
+	triggerEvent("Create", {
+		table: "POPULATION",
+		key: {
+			name: "Id",
+			column: "POPULATION_ID",
+			value: id
+		}
+	});
+	return id;
+};
+
+exports.update = function(entity) {
+	dao.update(entity);
+	triggerEvent("Update", {
+		table: "POPULATION",
+		key: {
+			name: "Id",
+			column: "POPULATION_ID",
+			value: entity.Id
+		}
+	});
+};
+
+exports.delete = function(id) {
+	dao.remove(id);
+	triggerEvent("Delete", {
+		table: "POPULATION",
+		key: {
+			name: "Id",
+			column: "POPULATION_ID",
+			value: id
+		}
+	});
+};
+
+exports.count = function() {
+	return dao.count();
+};
+
+exports.customDataCount = function() {
+	var resultSet = query.execute("SELECT COUNT(*) AS COUNT FROM POPULATION");
+	if (resultSet !== null && resultSet[0] !== null) {
+		if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
+			return resultSet[0].COUNT;
+		} else if (resultSet[0].count !== undefined && resultSet[0].count !== null) {
+			return resultSet[0].count;
+		}
+	}
+	return 0;
+};
+
+function triggerEvent(operation, data) {
+	producer.queue("genetyllis-app/Entities/Population/" + operation).send(JSON.stringify(data));
+}
