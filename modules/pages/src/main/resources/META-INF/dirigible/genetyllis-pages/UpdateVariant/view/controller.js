18db8b7feb20e49120889c9e4cf12bf17d4bbe4c
Yordan Velkov <jordan.v.jordanov@quanterall.com>
2022-07-14 17:07:42 +0300

    Fix for added trash code

---
+++ modules/pages/src/main/resources/META-INF/dirigible/genetyllis-pages/UpdateVariant/view/controller.js
@@ -0,0 +1,29 @@
+angular.module('page', []);
+angular.module('page').controller('PageController', function ($scope, $http) {
+    var api = "/services/v4/js/genetyllis-pages/UpdateVariant/view/updateRs.js";
+    var apiUpload = "/services/v4/js/genetyllis-pages/UpdateVariant/view/updateVariant.js";
+
+    $scope.getVariantId = function () {
+        $scope.variant = $scope.data.filter(v => {
+            return v.Id == $scope.variant.Id;
+        })
+
+        $scope.variantInfo = $scope.variant[0];
+
+        $scope.actionType = "show";
+
+        $http.post(apiUpload, JSON.stringify({ variantId: $scope.variantInfo.Id }))
+            .then(data => {
+                console.log("asd", data);
+            });
+    }
+
+    function load() {
+        $http.get(api)
+            .then(function (data) {
+                $scope.data = data.data;
+                console.log(data, " : patient");
+            });
+    }
+    load();
+});
